c7e8d4796c0a5f702a775f252e84dbae168b3a8a
karol.liszka <karol.liszka@onebi.eu>
2016-06-20 19:02:15 +0200

    Commit od slidera

---
+++ client/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('.collapsible').collapsible();
+    $('.slider').slider({
+        full_width: true,
+        indicators: false
+    });
+    $('.materialboxed').materialbox();
+});
