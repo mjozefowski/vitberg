e4f96570228ab34ebc8ef5807e2fd3fa3df230a8
karol.liszka <karol.liszka@onebi.eu>
2016-07-05 22:11:09 +0200

    Dodany slider w nowej templatce nazwa slider.html i slider.js

---
+++ client/common/pages/static/homePage/themes for bars/slider.js
@@ -0,0 +1,19 @@
+/**
+ * Created by SkylinR on 05.07.2016.
+ */
+
+Template.mainRedRight.onCreated(function () {
+
+})
+
+Template.mainRedRight.onRendered(function () {
+
+})
+
+Template.mainRedRight.helpers({
+
+})
+
+Template.mainRedRight.events({
+
+})
