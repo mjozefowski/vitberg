2bdb5ff61cfef97345bfad1433701dfb21cdfa86
karol.liszka <karol.liszka@onebi.eu>
2016-07-08 21:28:30 +0200

    trzecia templatka news'a

---
+++ client/common/pages/static/news/newsCenterPhoto.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) OneBi Sp. z o.o. All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by Karol Liszka, 06.07.16
+ */
+Template.newsCenterPhoto.onCreated(function () {
+
+})
+
+Template.newsCenterPhoto.onRendered(function () {
+
+
+})
+
+Template.newsCenterPhoto.helpers({
+
+})
+
+Template.newsCenterPhoto.events({
+
+})
+
