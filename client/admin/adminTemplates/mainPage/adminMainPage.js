b4a39897e00bbf02ce9d2bc490544665ed3bb1b6
maciej.jozefowski <mjozefowski@onebi.eu>
2016-07-17 18:33:23 +0200

    clean

--- client/admin/adminTemplates/mainPage/adminMainPage.js
+++ client/admin/adminTemplates/mainPage/adminMainPage.js
@@ -16,32 +16,19 @@ Template.adminMainPage.onCreated(function () {
 Template.adminMainPage.onRendered(function () {
     this.$('.blockDragItem').sortable({
         stop: function(e, ui) {
-            // get the dragged html element and the one before
-            //   and after it
             el = ui.item.get(0)
             before = ui.item.prev().get(0)
             after = ui.item.next().get(0)
 
-            // Here is the part that blew my mind!
-            //  Blaze.getData takes as a parameter an html element
-            //    and will return the data context that was bound when
-            //    that html element was rendered!
             if(!before) {
-                //if it was dragged into the first position grab the
-                // next element's data context and subtract one from the rank
                 newRank = Blaze.getData(after).order - 1
             } else if(!after) {
-                //if it was dragged into the last position grab the
-                //  previous element's data context and add one to the rank
                 newRank = Blaze.getData(before).order + 1
             }
             else
-            //else take the average of the two ranks of the previous
-            // and next elements
                 newRank = (Blaze.getData(after).order +
                     Blaze.getData(before).order)/2
 
-            //update the dragged Item's rank
             MainPage.update({_id: Blaze.getData(el)._id}, {$set: {order: newRank}})
         }
     })
@@ -103,7 +90,7 @@ Template.adminMainPage.events({
         var value = $("input[type='radio'][name='type']:checked").val();
 
         console.log(value)
-        t.selectedDocument.set(MainPage.insert({order:10,type:value, visible:true, showIcon:false, text:"Wpisz treść ..."}));
+        t.selectedDocument.set(MainPage.insert({order:10,type:value, visible:false, showIcon:false, text:"Wpisz treść ..."}));
         t.selectedTheme.set(value);
         t.insert.set(true)
     },
