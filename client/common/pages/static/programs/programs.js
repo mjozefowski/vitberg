08dc26664efab5837809955d419d49bc73defb15
SkylinR <skylinr7@gmail.com>
2016-07-31 21:11:54 +0200

    Program page 100% skonczona (responsywna, klikanie na obrazki)

--- client/common/pages/static/programs/programs.js
+++ client/common/pages/static/programs/programs.js
@@ -5,9 +5,16 @@
 
 Template.Programs.onCreated(function () {
 
+
+
 })
 
 Template.Programs.onRendered(function () {
+
+    $('.single-block-scroll').first().addClass('active-element-program');
+
+    var heightPhotoBlock = $('.photo-block').width();
+    $('.photo-block').css('height',heightPhotoBlock);
 
 })
 
@@ -17,9 +24,33 @@ Template.Programs.helpers({
 
 Template.Programs.events({
 
-    'click .single-block':function (e) {
-        $('.single-block').removeClass('active-element-program');
+    'click .single-block-scroll':function (e) {
+
+        $('.single-block-scroll').removeClass('active-element-program');
         $(e.target).addClass('active-element-program');
-    }
+        $('html, body').animate({
+            scrollTop: $("#programs-main-container").offset().top
+        }, 1000);
+
+    },
+
+    'click img':function(e){
+        var photo =$(e.target).attr('src');
+
+        $('.zoomed-container').children('img').attr('src',photo);
+        $('.zoomed-container').addClass('zoomed');
+    },
+
+    'click .zoomed':function(e){
+        $('.zoomed').removeClass('zoomed');
+    },
 
 })
+
+
+$( window ).resize(function() {
+
+    var heightPhotoBlock = $('.photo-block').width();
+    $('.photo-block').css('height',heightPhotoBlock);
+
+});
