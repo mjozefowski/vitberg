bf0e2120bf62ea3b13408fff69c556f10eeadd59
maciej.jozefowski <mjozefowski@onebi.eu>
2016-08-07 00:40:36 +0200

    creating subCategories

---
+++ client/admin/adminTemplates/categories/adminCategories.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) OneBi Sp. z o.o. All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by Maciej Józefowski, 06.08.16
+ */
+
+Modal.allowMultiple = true
+
+Template.adminCategories.onCreated(function () {
+    this.data= Template.currentData()
+})
+
+Template.adminCategories.onRendered(function () {
+
+})
+
+Template.adminCategories.helpers({
+    selectedDoc: function () {
+        var ti = Template.instance();
+        return ti.data;
+    }
+})
+
+Template.adminCategories.events({
+    'click .table-edit-button': function (e, t) {
+        console.log($(e.target).attr('id'))
+        Session.set('adminSelectedCategory', $(e.target).attr('id'));
+        Modal.show('adminCategoryEditModal', function () {
+            return Session.get('adminSelectedCategory')
+        })
+    }
+})
---
+++ client/admin/adminTemplates/categories/modals/adminCategoryEditModal.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) OneBi Sp. z o.o. All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by Maciej Józefowski, 06.08.16
+ */
+Modal.allowMultiple = true
+
+
+Template.adminCategoryEditModal.onCreated(function () {
+    this.data = Template.currentData();
+})
+
+Template.adminCategoryEditModal.onRendered(function () {
+
+})
+
+Template.adminCategoryEditModal.helpers({
+
+    selectedDoc: function () {
+        var ti = Template.instance();
+        return Categories.findOne(ti.data)
+    },
+    selector: function () {
+        var ti = Template.instance();
+        return {categoryId:ti.data}
+    }
+
+})
+
+Template.adminCategoryEditModal.events({
+
+    'click #addNewSubCategory': function (e,t) {
+        Session.set('adminSelectedSubCategory', t.data);
+        Modal.show('adminAddSubCategory', function () {
+            return Session.get('adminSelectedSubCategory')
+        })
+    }
+
+})
