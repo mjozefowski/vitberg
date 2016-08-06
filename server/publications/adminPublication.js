71aa3878191029a6a3fef5c323fe434132047b97
maciej.jozefowski <mjozefowski@onebi.eu>
2016-08-06 21:05:51 +0200

    fix, subscription restricted

--- server/publications/adminPublication.js
+++ server/publications/adminPublication.js
@@ -7,7 +7,7 @@
 
 Meteor.publish('adminPublication', function () {
 
-    if(this.userId){
+    if(this.userId && Roles.userIsInRole(this.userId,['editor','admin','contractor'])){
         return [News.find(),Categories.find(),SubCategories.find(),Images.find(), MainPage.find(), Markers.find(), Icons.find(), TemplatesForMainClickable.find(),ClickableItemsTemplate.find(),Notifications.find(),NotificationsUser.find(),Opinions.find(), ImagesFromClient.find()]
     }
 
